206b2ebf9a22f33377ba82060427f9109aa8a8bb
Ha Tran <hatran84@outlook.com>
2018-01-22 18:14:49 -0800

    main scraping function

--- webscraping.py
+++ webscraping.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from collections import OrderedDict
 from helper import *
+from upload_data_to_S3 import *
+import boto3
 
 
 
@@ -15,3 +17,6 @@ tech_companies_lst = ['Apple', 'Google', 'Microsoft', 'Amazon', 'Intel', 'IBM', 
                       'Salesforce', 'Kaiser Permanente', 'Indeed', 'Redfin', 'Zillow', 'Expedia', 'T-mobile',
                      'Tableau', 'eBay','Allstate', 'KPMG', 'Nordstrom', 'Boeing', 'Starbuck','University of Washington',
                      'NOKIA','JLL', 'Adobe']
+
+for cp in tech_companies_lst:
+    main_scraping_function(cp)
