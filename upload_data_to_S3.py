2e63d977d5a9be2d09f77f698d16158ed3debfb8
Ha Tran <hatran84@outlook.com>
2018-01-22 18:50:11 -0800

    change upload_data

--- upload_data_to_S3.py
+++ upload_data_to_S3.py
@@ -2,9 +2,14 @@ import boto3
 
 def upload_data(file_name):
     s3 = boto3.resource('s3')
-    bucket_name = "capstone_raw_data"
+    bucket_name = "capstone_raw_data_ha_galvanize"
     try:
         s3.meta.client.upload_file(file_name, bucket_name, file_name)
 
     except Exception as e:
         print(e)  # Note: BucketAlreadyOwnedByYou means you already created the bucket.
+
+
+if __name__=="__main__":
+    s3 = boto3.resource('s3')
+    upload_data(FileName)
