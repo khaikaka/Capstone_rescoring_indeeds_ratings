42e17d606e6f76e5a34278e7aa9de378a75a0ae6
Ha Tran <hatran84@outlook.com>
2018-01-25 20:48:04 -0800

    extra cleaning added

--- extra_cleaning_obj1.py
+++ extra_cleaning_obj1.py
@@ -13,6 +13,7 @@ import warnings
 warnings.filterwarnings("ignore")
 from sklearn.model_selection import train_test_split
 from extra_cleaning_obj1 import *
+import pickle
 
 
 
@@ -25,6 +26,7 @@ def pulling_data(data_path):
     data = pd.read_csv(data_path)
     data = main_cleaning_function(data)
     return data
+
 
 
 def preparing_data_logistic_regression(data, num_clusters=6):
@@ -47,11 +49,14 @@ def preparing_data_logistic_regression(data, num_clusters=6):
     data_no_text = data.drop(columns=['all_text'], axis=1)
 
     data_with_text = pd.concat([data, text_label], axis=1)
+    data_with_text = data_with_text[data_with_text['text_groups'].isnull() == False]
     data_with_text  = pd.concat([data, pd.get_dummies(text_label['text_groups'], prefix = 'group')], axis=1)
     data_with_text  = data_with_text.drop(columns=['all_text'], axis=1)
 
-    data_no_text = data_no_text[data_no_text['overall_sc_4'] != 1][data_no_text['overall_sc_3'] != 1][data_no_text['contract'] == 1]
-    data_with_text = data_with_text[data_with_text['overall_sc_4'] != 1][data_with_text['overall_sc_3'] != 1][data_with_text['contract'] == 1]
+    data_with_text = data_with_text[data_with_text['group_5'].isnull()==False]
+    # data_no_text = data_no_text[data_no_text['overall_sc_4'] != 1][data_no_text['overall_sc_3'] != 1][data_no_text['contract'] == 0]
+    # data_with_text = data_with_text[data_with_text['overall_sc_4'] != 1][data_with_text['overall_sc_3'] != 1][data_with_text['contract'] == 0]
+
 
     return data_no_text, data_with_text
 
@@ -61,49 +66,51 @@ def main_functions_clean_after_KMeans(data_path, num_cltrs=8):
     return data_no_text, data_with_text
 
 
-def train_test_data_splitting_after_KMeans_logistic(data_path, n_groups=6):
-    data_no_text, data_with_text = main_functions_clean_after_KMeans(data_path, n_groups)
-
-    y_no_text = data_no_text['former_current']
-    X_no_text = data_no_text.drop(columns=['former_current', 'Google', 'overall_sc', 'management_sc', 'other_states'], axis=1)
-
-    y_with_text = data_with_text['former_current']
-    X_with_text = data_with_text.drop(columns=['former_current', 'Google', 'overall_sc', 'management_sc', 'other_states', 'group_0'], axis=1)
-
-
-    X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test = train_test_split(X_no_text, y_no_text, test_size=0.4)
-    X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test = train_test_split(X_with_text, y_with_text, test_size=0.4)
-
-    return X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test, \
-           X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test
-
-def train_test_data_splitting_after_KMeans(data_path, n_groups=6):
-    data_no_text, data_with_text = main_functions_clean_after_KMeans(data_path, n_groups)
-
-    y_no_text = data_no_text['former_current']
-    X_no_text = data_no_text.drop(columns=['former_current'], axis=1)
-
-    y_with_text = data_with_text['former_current']
-    X_with_text = data_with_text.drop(columns=['former_current'], axis=1)
-
-
-    X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test = train_test_split(X_no_text, y_no_text, test_size=0.2)
-    X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test = train_test_split(X_with_text, y_with_text, test_size=0.2)
-
-    return X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test, \
-           X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test
+# def train_test_data_splitting_after_KMeans_logistic(data_path, n_groups=6):
+#     data_no_text, data_with_text = main_functions_clean_after_KMeans(data_path, n_groups)
+#
+#     y_no_text = data_no_text['former_current']
+#     X_no_text = data_no_text.drop(columns=['former_current', 'Google', 'overall_sc', 'management_sc', 'other_states'], axis=1)
+#
+#     y_with_text = data_with_text['former_current']
+#     X_with_text = data_with_text.drop(columns=['former_current', 'Google', 'overall_sc', 'management_sc', 'other_states', 'group_0'], axis=1)
+#
+#
+#     X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test = train_test_split(X_no_text, y_no_text, test_size=0.4)
+#     X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test = train_test_split(X_with_text, y_with_text, test_size=0.4)
+#
+#     return X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test, \
+#            X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test
+#
+# def train_test_data_splitting_after_KMeans(data_path, n_groups=6):
+#     data_no_text, data_with_text = main_functions_clean_after_KMeans(data_path, n_groups)
+#
+#     y_no_text = data_no_text['former_current']
+#     X_no_text = data_no_text.drop(columns=['former_current'], axis=1)
+#
+#     y_with_text = data_with_text['former_current']
+#     X_with_text = data_with_text.drop(columns=['former_current'], axis=1)
+#
+#
+#     X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test = train_test_split(X_no_text, y_no_text, test_size=0.2)
+#     X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test = train_test_split(X_with_text, y_with_text, test_size=0.2)
+#
+#     return X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test, \
+#            X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test
 
 
 def train_test_data_splitting_after_KMeans_logistic_sub(data_path, n_groups=6):
     data_no_text, data_with_text = main_functions_clean_after_KMeans(data_path, n_groups)
-    data_no_text = data_no_text[data_no_text['overall_sc'].isin([1,2,5])]
-    data_with_text = data_with_text[data_no_text['overall_sc'].isin([1,2,5])]
 
     y_no_text = data_no_text['former_current']
-    X_no_text = data_no_text.drop(columns=['former_current', 'overall_sc', 'management_sc', 'balance_sc', 'other_states'], axis=1)
+    X_no_text = data_no_text.drop(columns=['former_current', 'benefit_sc_1', 'security_sc_1', \
+                                           'overall_sc_1', 'management_sc_1', 'culture_sc_1', 'balance_sc_1', 'other_states',\
+                                            'Google', 'engineer', 'month_1', 'year_2016' ], axis=1)
 
     y_with_text = data_with_text['former_current']
-    X_with_text = data_with_text.drop(columns=['former_current', 'overall_sc', 'management_sc', 'balance_sc', 'other_states', 'group_0'], axis=1)
+    X_with_text = data_with_text.drop(columns=['former_current', 'benefit_sc_1', 'security_sc_1', \
+                                           'overall_sc_1', 'management_sc_1', 'culture_sc_1', 'balance_sc_1', 'other_states',\
+                                            'Google', 'engineer', 'month_1', 'year_2016' ], axis=1)
 
 
     X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test = train_test_split(X_no_text, y_no_text, test_size=0.4)
@@ -119,10 +126,10 @@ def train_test_data_splitting_after_KMeans_sub(data_path, n_groups=6):
     #data_with_text = data_with_text[data_no_text['overall_sc'].where((data_no_text['overall_sc']!=3 or data_no_text['overall_sc']!=4) and data_no_text['engineer'] == 1)]
 
     y_no_text = data_no_text['former_current']
-    X_no_text = data_no_text.drop(columns=['former_current', 'company_name'], axis=1)
+    X_no_text = data_no_text.drop(columns=['former_current'], axis=1)
 
     y_with_text = data_with_text['former_current']
-    X_with_text = data_with_text.drop(columns=['former_current', 'company_name'], axis=1)
+    X_with_text = data_with_text.drop(columns=['former_current'], axis=1)
 
     X_no_text_train, X_no_text_test, y_no_text_train, y_no_text_test = train_test_split(X_no_text, y_no_text, test_size=0.2)
     X_with_text_train, X_with_text_test, y_with_text_train, y_with_text_test = train_test_split(X_with_text, y_with_text, test_size=0.2)
