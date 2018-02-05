f40d6dc9e0155c2507f587cb81796f93f9bac87b
Ha Tran <hatran84@outlook.com>
2018-02-04 21:19:58 -0800

    app.py

--- website/app.py
+++ website/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('capstone.html')
+    return render_template('index.html')
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
